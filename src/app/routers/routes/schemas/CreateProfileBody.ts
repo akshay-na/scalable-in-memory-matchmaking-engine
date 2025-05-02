b0a6050835cceb3e10822878218601d27ec9b278
akshay-na <akshayna04@gmail.com>
2025-05-02 22:21:47 +0530

    chore: validating interest pool

--- src/app/routers/routes/schemas/CreateProfileBody.ts
+++ src/app/routers/routes/schemas/CreateProfileBody.ts
@@ -1,6 +1,8 @@
 import { UUID } from "@akshay-na/exoframe/lib/common/UUID";
 import { z } from "@akshay-na/exoframe/lib/zod/ZodUtils";
 
+import { INTEREST_POOL } from "@/constants/Constants";
+
 const locationSchema = z.object({
   lat: z.number().min(-90).max(90),
   lon: z.number().min(-180).max(180),
@@ -11,7 +13,9 @@ export const createProfileSchema = z.object({
   age: z.number().min(0),
   gender: z.enum(["M", "F", "Other"]),
   location: locationSchema,
-  interests: z.array(z.string()),
+  interests: z.array(z.enum(INTEREST_POOL)).max(5, {
+    message: "You can select a maximum of 5 interests",
+  }),
 });
 
 export type ProfileData = z.infer<typeof createProfileSchema>;
--- src/constants/Constants.ts
+++ src/constants/Constants.ts
@@ -88,4 +88,4 @@ export const INTEREST_POOL = [
   "crypto",
   "entrepreneurship",
   "economics",
-];
+] as const;
