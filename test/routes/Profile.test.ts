85f2ebaa9c46be281d16c437a9d311991b97c3e6
akshay-na <akshayna04@gmail.com>
2025-05-03 11:37:05 +0530

    fix: userId in test case

--- test/routes/Profile.test.ts
+++ test/routes/Profile.test.ts
@@ -1,11 +1,13 @@
+import { UUID } from "@akshay-na/exoframe/lib/common/UUID";
 import request from "supertest";
 
 describe("POST /api/v1/profile", () => {
   it("should create profile", async () => {
+    const userId = UUID.randomUUID();
     const res = await request(app)
       .post("/api/v1/profile")
       .send({
-        id: "user123",
+        id: userId,
         age: 28,
         gender: "M",
         location: { lat: 13.7563, lon: 100.5018 },
@@ -13,14 +15,14 @@ describe("POST /api/v1/profile", () => {
       })
       .set("Accept", "application/json");
     expect(res.status).toBe(200);
-    expect(res.body.data.id).toBe("user123");
+    expect(res.body.data.id).toBe(userId);
   });
 
   it("should respond with error when required fields are missing", async () => {
     const res = await request(app)
       .post("/api/v1/profile")
       .send({
-        id: "user123", // Missing age, gender, location, and interests
+        age: "user123", // Missing age, gender, location, and interests
       })
       .set("Accept", "application/json");
     expect(res.status).toBe(400); // Assuming these fields are required
