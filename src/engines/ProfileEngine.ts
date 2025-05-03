64b2dfadf16f6612159f4896182c3b7ac4266f40
akshay-na <akshayna04@gmail.com>
2025-05-03 11:25:47 +0530

    fix: import issue

--- src/engines/ProfileEngine.ts
+++ src/engines/ProfileEngine.ts
@@ -1,8 +1,9 @@
 import { ObjectUtils } from "@akshay-na/exoframe/lib/common/ObjectUtils";
+import { RuntimeError } from "@akshay-na/exoframe/lib/common/RuntimeError";
 import { UUID } from "@akshay-na/exoframe/lib/common/UUID";
+
 import { faker } from "@faker-js/faker";
 
-import { RuntimeError } from "src/lib/src/lib/common/RuntimeError";
 import { ProfileData } from "../app/types";
 import { INTEREST_POOL } from "../constants/Constants";
 import { ProfileController } from "../controllers/ProfileController";
